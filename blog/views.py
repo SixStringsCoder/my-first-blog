499af9c869f3d80781346216944edae9f19251b4
SixStringsCoder <rshanlon@gmail.com>
2017-07-27 20:02:02 -0700

    added comments to views

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
+    """
+    published blog posts sorted by published date
+
+    """
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     context = {'posts': posts}
     return render(request, 'blog/post_list.html', context)
@@ -21,6 +25,10 @@ def post_detail(request, pk):
 
 @login_required
 def post_new(request):
+    """
+    creates new blog post
+
+    """
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
@@ -37,6 +45,10 @@ def post_new(request):
 
 @login_required
 def post_edit(request, pk):
+    """
+    edit blog post
+
+    """
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
@@ -54,12 +66,20 @@ def post_edit(request, pk):
 
 @login_required
 def post_draft_list(request):
+    """
+    unpublished blog posts
+
+    """
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     context = {'posts': posts}
     return render(request, 'blog/post_draft_list.html', context)
 
 
 def post_publish(request, pk):
+    """
+    publishes unpublished post
+
+    """
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_list')
@@ -67,12 +87,20 @@ def post_publish(request, pk):
 
 @login_required
 def post_remove(request, pk):
+    """
+    removes post both published and unpublished
+
+    """
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
 
 
 def add_comment_to_post(request, pk):
+    """
+    add comment to a published post
+
+    """
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -86,14 +114,24 @@ def add_comment_to_post(request, pk):
     context = {'form': form}
     return render(request, 'blog/add_comment_to_post.html', context)
 
+
 @login_required
 def comment_approve(request, pk):
+    """
+    approves comment to be viewed by public
+
+    """
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
 
+
 @login_required
 def comment_remove(request, pk):
+    """
+    disapproves comment and removes it
+
+    """
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
